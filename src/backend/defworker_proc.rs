1a4845edf7cb67f4d6062aa97af2b6239c10eafa
Nicolas H. Chung <1310652199@qq.com>
2024-08-23 21:12:25 -0400

    test

--- src/backend/defworker_proc.rs
+++ src/backend/defworker_proc.rs
@@ -3,6 +3,6 @@ use tokio::sync::mpsc;
 pub async fn defworker_proc(mut receive_channel: mpsc::Receiver<i32>) {
     println!("enter defwroker");
     while let Some(val) = receive_channel.recv().await {
-        println!("got {}", val);
+        todo!()
     }
 }
